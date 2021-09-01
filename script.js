3d5eecffdc3baa27607cefbc9524952ba8875429
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-09-01 16:59:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log(
   `%cSTOP! This is a developer tool. Do not use this if you do not know what you're doing.`,
-  "line-height: 5; font-weight: bold; color: red"
+  "line-height: 5; font-weight: 900; color: red"
 )
